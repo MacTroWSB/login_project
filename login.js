c7a16998f0ac0238d581dfb87fcd203f19c88fbb
MacTroWSB <105553556+MacTroWSB@users.noreply.github.com>
2022-11-26 10:40:39 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
