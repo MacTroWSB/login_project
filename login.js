3de42be4eb6eb9e802beec3d1aa88f0448b0ceef
MacTroWSB <105553556+MacTroWSB@users.noreply.github.com>
2022-11-27 19:23:54 +0100

    Update login.js

--- login.js
+++ login.js
@@ -2,15 +2,22 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+//wskazanie elementow ktore beda wykorzystywane
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "21" && password === "37") {
+//opis dzialania klikniecia w przycisk logowania
+
+    if (username === "user" && password === "password") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
+    
+//opis dzialania strony w przypadku podania dobrych lub zlych danych
+    
 })
